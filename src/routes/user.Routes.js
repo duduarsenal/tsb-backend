7536e2c4ab977b541dd23bcdf00bf22d1c23eb0f
Dudu <edudocross@gmail.com>
2023-10-05 19:30:18 -0300

    fix: change routes permissions

--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -5,16 +5,18 @@ const UserController = require('../controllers/UserController')
 const confirmToken = require('../middlewares/confirmToken');
 const verifyRole = require('../middlewares/verifyRole');
 
+// Free Routes
 router.post('/register', (req, res) => UserController.createUser(req, res))
 router.post('/auth', (req, res) => UserController.authUser(req, res))
 
-router.get('/getall', confirmToken, (req, res) => UserController.getAllUsers(req, res))
+// Authenthicated Routes
 router.get('/getone', confirmToken, (req, res) => UserController.getUser(req, res))
-
-router.patch('/', confirmToken, (req, res) => UserController.updateUser(req, res))
 router.patch('/cregister', confirmToken, (req, res) => UserController.completeRegister(req, res))
-
 router.delete('/', confirmToken, (req, res) => UserController.deleteUser(req, res))
+
+// Admin Routes
+router.get('/getall', confirmToken, verifyRole, (req, res) => UserController.getAllUsers(req, res))
+router.patch('/', confirmToken, verifyRole, (req, res) => UserController.updateUser(req, res))
 router.delete('/:id', confirmToken, verifyRole, (req, res) => UserController.deleteUser(req, res))
 
 module.exports = router;
